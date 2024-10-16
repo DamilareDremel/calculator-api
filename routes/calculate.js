60aa3820e38ab004c2bfac398f5eb088fc0cf931
DamilareDremel <dremelerablogdeb@gmail.com>
2024-10-16 11:06:12 +0100

    Added solved errors

--- routes/calculate.js
+++ routes/calculate.js
@@ -1,8 +1,20 @@
+// routes/calculateRoutes.js
 const express = require('express');
-const calculate = require('./calculateController'); // Adjust path as necessary
-
 const router = express.Router();
+const calculateController = require('../controllers/calculateController');
 
-router.post('/api/calculate/:operation', calculate);
+// Basic operations
+router.post('/add', calculateController.add);
+router.post('/subtract', calculateController.subtract);
+router.post('/multiply', calculateController.multiply);
+router.post('/divide', calculateController.divide);
+
+// Rounding operations
+router.post('/nearest-10s', calculateController.roundToTens);
+router.post('/nearest-100s', calculateController.roundToHundreds);
+router.post('/nearest-1000s', calculateController.roundToThousands);
+router.post('/nearest-10th', calculateController.roundToTenths);
+router.post('/nearest-100th', calculateController.roundToHundredths);
+router.post('/nearest-1000th', calculateController.roundToThousandths);
 
 module.exports = router;
